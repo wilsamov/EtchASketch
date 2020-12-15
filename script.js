cabb89414e063e5ce7dcde55a8153a0f87fcf526
wilsamov <wilsamov@gmail.com>
2020-12-15 08:47:46 -0800

    Simplified UI. Divs now appear as grid cells. Event listeners added.

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-//global variables
 const container = document.querySelector('#container');
-let gridSize = 16;
+let gridSize;
+const sixteen = document.getElementById('size16');
+const thirtytwo = document.getElementById('size32');
+const clearGrid = document.getElementById('clearSelect');
 
-//function that creates specified number of
+sixteen.addEventListener('click', (e) => {
+    gridSize = 16;
+    addDivs(gridSize);
+});
+
+thirtytwo.addEventListener('click', (e) => {
+    gridSize = 32;
+    addDivs(gridSize);
+});
+
+clearGrid.addEventListener('click', (e) => {
+    clearSquares();
+});
+
 function addDivs (gridSize) {
+  container.style.setProperty('--grid-rows', gridSize);
+  container.style.setProperty('--grid-cols', gridSize);
 
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    container.appendChild(div);
+  }hoverColor();
 }
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const div = document.createElement('div');
-  div.classList.add('grid-item');
-  container.appendChild(div);
-}
-
-//80's colors
-let eightiesColor = function() {
-  let colorList = ["00ffbf", "#00ffff", "#00bfff", "#0080ff", "#0040ff", "#0000ff", "#4000ff", "#8000ff", "#bf00ff", "#ff00ff", "#ff00bf", "#ff0080"];
-  let color;
-  let randomIndex = Math.floor(Math.random() * 11);
-  color = colorList[randomIndex];
-  console.log(color)
-  return color;
+function hoverColor() {
+  let items = document.querySelectorAll('.grid-item');
+  items.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.style.backgroundColor = `${randomColor()}`;
+    });
+  });
 }
 
 //function that creates a random color
@@ -32,13 +47,3 @@ let randomColor = function() {
     }
     return color;
 }
-
-//function that changes div color uponed being hovered
-function hoverColor() {
-  let item = document.querySelector('.grid-item');
-  item.addEventListener('hover', (e) => {
-    item.style.backgroundColor = `${randomColor}`;
-  });
-}
-
-eightiesColor();
